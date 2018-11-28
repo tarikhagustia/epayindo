61e73a56411d037acd416174f86b9acc9ee38a89
Tarikh Agustia <agustia.tarikh150@gmail.com>
2018-11-28 20:05:39 +0700

    Update Endpoint lalasung

--- src/Api.php
+++ src/Api.php
@@ -3,7 +3,7 @@ namespace Epayindo;
 
 class Api {
   public static function post($end_point, $data){
-      $host = "http://my.epayindo.com/sci";
+      $host = "https://epayindo.lalasung.com/sci";
       $ch = curl_init();
       $base_url = $host.$end_point;
       curl_setopt($ch, CURLOPT_URL, $base_url);
